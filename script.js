a97d3240db6ea4826327f92253a7a538a072641c
mariaeduardamoraesarzao11042007 <128145141+mariaeduardamoraesarzao11042007@users.noreply.github.com>
2024-06-24 08:37:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+]const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+enunciado: "Qual o papel da IA na medicina?",
+alternativas: [
+"Auxiliar diagnósticos",
+"Alternativa 2"
+"Alternativa 3"
+"Alternativa 4"
+]
+},
+{
+enunciado: "Quais são os benefícios da iA na indústria?",
+alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+"Alternativa 3"
+"Alternativa 4"
+]
+},
+{
+enunciado: "Como a IA está sendo aplicada na indústria aplicada na indústria financeira?,
+alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+"Alternativa 3"
+"Alternativa 4"
+]
+},
+{
